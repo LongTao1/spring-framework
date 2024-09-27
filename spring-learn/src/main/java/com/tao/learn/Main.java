1bb68694fbb32ac71702976287159205a9689c0a
30043-龙涛 <javalongtao@163.com>
2024-09-27 15:51:44 +0800

    spring源码阅读环境搭建

---
+++ spring-learn/src/main/java/com/tao/learn/Main.java
@@ -0,0 +1,12 @@
+package com.tao.learn;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		User user = (User)context.getBean("user");
+		System.out.println(user.toString());
+	}
+}
